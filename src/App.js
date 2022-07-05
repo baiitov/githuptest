5b7218b9bea8aca2691a21f073801e7b39adbc0a
Halida81 <halidas81@gmail.com>
2022-07-05 18:08:57 +0600

    added input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
       <h2>bilal</h2>
+      <Input/>
     </div>
   );
 }
